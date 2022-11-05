2ecb2b058f0a765abbb3a6a97ab904b742a06ef8
Nikolay <nk@jquad.de>
2022-11-05 21:42:57 +0100

    Lab 9: Responding to an Event

--- src/projects/ProjectCard.tsx
+++ src/projects/ProjectCard.tsx
@@ -1,8 +1,7 @@
-import { Project } from './Project';
-import React from 'react';
+import { Project } from "./Project";
 
 function formatDescription(description: string): string {
-  return description.substring(0, 60) + '...';
+  return description.substring(0, 60) + "...";
 }
 
 interface ProjectCardProps {
@@ -11,6 +10,9 @@ interface ProjectCardProps {
 
 function ProjectCard(props: ProjectCardProps) {
   const { project } = props;
+  const handleEditClick = (projectBeingEdited: Project) => {
+    console.log(projectBeingEdited);
+  };
   return (
     <div className="card">
       <img src={project.imageUrl} alt={project.name} />
@@ -20,6 +22,15 @@ function ProjectCard(props: ProjectCardProps) {
         </h5>
         <p>{formatDescription(project.description)}</p>
         <p>Budget : {project.budget.toLocaleString()}</p>
+        <button
+          className="bordered"
+          onClick={() => {
+            handleEditClick(project);
+          }}
+        >
+          <span className="icon-edit "></span>
+          Edit
+        </button>
       </section>
     </div>
   );

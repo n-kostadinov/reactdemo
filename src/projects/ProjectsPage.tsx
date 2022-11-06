5699f2741e9d67d591d2de460cb38850a17443b9
Nikolay <nk@jquad.de>
2022-11-06 15:42:46 +0100

    Lab 18: HTTP GET

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,23 +1,61 @@
 import { MOCK_PROJECTS } from "./MockProjects";
 import ProjectList from "./ProjectList";
-import {Project} from "./Project";
-import{ useState } from 'react';
+import { Project } from "./Project";
+import React, { Fragment, useState, useEffect } from "react";
+import { projectAPI } from "./projectAPI";
 
 function ProjectsPage() {
+  const [projects, setProjects] = useState<Project[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | undefined>(undefined);
 
-  const [projects, setProjects] = useState<Project[]>(MOCK_PROJECTS);
+  useEffect(() => {
+    async function loadProjects() {
+      try {
+        setLoading(true);
+        const data = await projectAPI.get(1);
+        setError("");
+        setProjects(data);
+      } catch (e) {
+        if (e instanceof Error) {
+          setError(e.message);
+        }
+      } finally {
+        setLoading(false);
+      }
+    }
+    loadProjects();
+  }, []);
 
   const saveProject = (project: Project) => {
-    let updatedProjects = projects.map( (p:Project) => {
+    let updatedProjects = projects.map((p: Project) => {
       return p.id === project.id ? project : p;
     });
-    setProjects(updatedProjects)
+    setProjects(updatedProjects);
   };
 
   return (
     <>
+      {error && (
+        <div className="row">
+          <div className="card large error">
+            <section>
+              <p>
+                <span className="icon-alert inverse "></span>
+                {error}
+              </p>
+            </section>
+          </div>
+        </div>
+      )}
       <h1>Projects</h1>
       <ProjectList onSave={saveProject} projects={projects} />
+      {loading && (
+        <div className="center-page">
+          <span className="spinner primary"></span>
+          <p>Loading...</p>
+        </div>
+      )}
     </>
   );
 }
